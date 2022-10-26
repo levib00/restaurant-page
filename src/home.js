a92adda1791dfe9bbf6284afd8d68019b2404b3d
levib00 <levib8@hotmail.com>
2022-10-25 21:02:35 -0400

    Add home page and buttons to switch

---
+++ src/home.js
@@ -0,0 +1,32 @@
+const generateHomePage = () => {
+    
+
+    const heading = document.createElement('h1');
+    heading.textContent =  'Pepe\'s Pizza';
+    heading.setAttribute('id','heading')
+
+    const homeContent = document.createElement('div')
+    homeContent.setAttribute('id','flex-hor-center')
+
+    const content = document.getElementById('content')
+
+    content.appendChild(heading);
+    content.appendChild(homeContent);
+
+    const mainPic = document.createElement('img');
+    mainPic.setAttribute('id','main-pic')
+    mainPic.setAttribute('src', 'https://media.blogto.com/articles/20160524-piandco2048-06.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70');
+
+    const mainCaption = document.createElement('h3');
+    mainCaption.textContent = 'Fresh, Fired, Fantastic Pizza!'
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('id','paragraph')
+    paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum turpis. Sed fringilla lacus sed massa interdum congue. Pellentesque dui arcu, accumsan in aliquet nec, facilisis at magna.'
+    
+
+    homeContent.appendChild(mainPic)
+    homeContent.appendChild(mainCaption)
+    homeContent.appendChild(paragraph)
+};
+export default generateHomePage()
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import generateHomePage from './home'
+generateHomePage
+
